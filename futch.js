2e49d2b7da517bb9329f1ca5d1136e518a618518
Jordan <jordanbrennan@me.com>
2018-11-14 15:18:28 -0800

    Update futch.js

--- futch.js
+++ futch.js
@@ -22,21 +22,25 @@ window.futch = (function () {
     };
 
     function makeRequest(url, options) {
-        if (options.id) url += `/${options.id}`;
+        // Some url work
         url = new URL(url);
+        if (options.id) url.pathname += (url.pathname.endsWith('/') ? '' : '/') + options.id;
         if (options.params) Object.keys(options.params).forEach(key => url.searchParams.append(key, options.params[key]));
+
+        // You're welcome
         if (options.body) options.body = JSON.stringify(options.body);
-        
-        // Set init defaults, override when provided
+
+        // Set init defaults, override if provided
         options.credentials = options.credentials || 'include';
         options.headers = {'Content-type': 'application/json; charset=UTF-8'};
 
+        // All ready, send the request
         return fetch(url.toString(), options).then(res => {
             if (res.ok) {
                 return res.json();
             }
             else {
-                throw new Error('Network response was not ok.');
+                throw new Error('Network response was not ok, i.e. non-200.');
             }
         })
     }
